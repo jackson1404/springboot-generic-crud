be42173127088f6351c694050ca53edc2c1876fc
Win Min Thant <jackson14042004@gmail.com>
2025-07-21 09:05:12 +0630

    first commit

---
+++ src/main/java/com/jackson/generic_crud_testing/GenericCrudTestingApplication.java
@@ -0,0 +1,13 @@
+package com.jackson.generic_crud_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericCrudTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericCrudTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jackson/generic_crud_testing/GenericCrudTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackson.generic_crud_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericCrudTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
