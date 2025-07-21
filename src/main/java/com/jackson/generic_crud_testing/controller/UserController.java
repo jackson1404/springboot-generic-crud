716b6563f35ebb427798ad63b8ef11b17121e59e
jackson1404 <jackson14042004@gmail.com>
2025-07-21 23:01:02 +0630

    modify user controller

--- src/main/java/com/jackson/generic_crud_testing/controller/UserController.java
+++ src/main/java/com/jackson/generic_crud_testing/controller/UserController.java
@@ -2,10 +2,17 @@ package com.jackson.generic_crud_testing.controller;
 
 import com.jackson.generic_crud_testing.dto.UserDto;
 import com.jackson.generic_crud_testing.model.UserEntity;
+import com.jackson.generic_crud_testing.service.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController extends CrudController<UserEntity, UserDto, Long> {
+
+    public UserController(UserService userService){
+        super(userService);
+    }
+
+
 }
