23984d34f39d475b162e3d0f6f500e43fdd9b1ef
Win Min Thant <jackson14042004@gmail.com>
2025-07-22 11:27:04 +0630

    added global cors and securtiy config

---
+++ src/main/java/com/jackson/generic_crud_testing/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+/***************************************************************
+ * Author       :	 
+ * Created Date :	
+ * Version      : 	
+ * History  :	
+ * *************************************************************/
+package com.jackson.generic_crud_testing.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * SecurityConfig Class.
+ * <p>
+ * </p>
+ *
+ * @author
+ */
+
+@Configuration
+public class SecurityConfig {
+    
+
+
+}
